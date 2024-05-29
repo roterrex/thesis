7a65372c7f4b47849d559be85a83015c62042ff7
roterrex <joel.roterex@gmail.com>
2024-05-29 13:45:01 +1000

    small steps towards using my data

--- PlanGen/main.py
+++ PlanGen/main.py
@@ -1,3 +1,4 @@
+from PlanGen.util import util
 import tensorflow as tf
 import os
 import pathlib
@@ -35,53 +36,6 @@ plt.figure()
 plt.imshow(sample_image)
 plt.show()
 
-
-# The facade training set consist of 400 images
-BUFFER_SIZE = 400
-# The batch size of 1 produced better results for the U-Net in the original pix2pix experiment
-BATCH_SIZE = 1
-# Each image is 256x256 in size
-IMG_WIDTH = 64
-IMG_HEIGHT = 64
-
-
-def resize(input_image, real_image, height, width):
-  input_image = tf.image.resize(input_image, [height, width],
-                                method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-  real_image = tf.image.resize(real_image, [height, width],
-                               method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-
-  return input_image, real_image
-
-def random_crop(input_image, real_image):
-  stacked_image = tf.stack([input_image, real_image], axis=0)
-  cropped_image = tf.image.random_crop(
-      stacked_image, size=[2, IMG_HEIGHT, IMG_WIDTH, 3])
-
-  return cropped_image[0], cropped_image[1]
-
-# Normalizing the images to [-1, 1]
-def normalize(input_image, real_image):
-  input_image = (input_image / 127.5) - 1
-  real_image = (real_image / 127.5) - 1
-
-  return input_image, real_image
-
-@tf.function()
-def random_jitter(input_image, real_image):
-  # Resizing to 286x286
-  input_image, real_image = resize(input_image, real_image, 286, 286)
-
-  # Random cropping back to 256x256
-  input_image, real_image = random_crop(input_image, real_image)
-
-  if tf.random.uniform(()) > 0.5:
-    # Random mirroring
-    input_image = tf.image.flip_left_right(input_image)
-    real_image = tf.image.flip_left_right(real_image)
-
-  return input_image, real_image
-
 def load(image_file):
   # Read and decode an image file to a uint8 tensor
   image = tf.io.read_file(image_file)
@@ -103,16 +57,16 @@ def load(image_file):
 
 def load_image_train(image_file):
   input_image, real_image = load(image_file)
-  input_image, real_image = random_jitter(input_image, real_image)
-  input_image, real_image = normalize(input_image, real_image)
+  input_image, real_image = util.random_jitter(input_image, real_image, conf)
+  input_image, real_image = util.normalize(input_image, real_image)
 
   return input_image, real_image
 
 def load_image_test(image_file):
   input_image, real_image = load(image_file)
-  input_image, real_image = resize(input_image, real_image,
-                                   IMG_HEIGHT, IMG_WIDTH)
-  input_image, real_image = normalize(input_image, real_image)
+  input_image, real_image = util.resize(input_image, real_image, 
+                                        conf['LayoutGan']['ImgSize'])
+  input_image, real_image = util.normalize(input_image, real_image)
 
   return input_image, real_image
 
@@ -120,15 +74,15 @@ def load_image_test(image_file):
 train_dataset = tf.data.Dataset.list_files(str(PATH / 'train/*.jpg'))
 train_dataset = train_dataset.map(load_image_train,
                                   num_parallel_calls=tf.data.AUTOTUNE)
-train_dataset = train_dataset.shuffle(BUFFER_SIZE)
-train_dataset = train_dataset.batch(BATCH_SIZE)
+train_dataset = train_dataset.shuffle(conf['LayoutGan']['BufferSize'])
+train_dataset = train_dataset.batch(conf['LayoutGan']['BatchSize'])
 
 try:
   test_dataset = tf.data.Dataset.list_files(str(PATH / 'test/*.jpg'))
 except tf.errors.InvalidArgumentError:
   test_dataset = tf.data.Dataset.list_files(str(PATH / 'val/*.jpg'))
 test_dataset = test_dataset.map(load_image_test)
-test_dataset = test_dataset.batch(BATCH_SIZE)
+test_dataset = test_dataset.batch(conf['LayoutGan']['BatchSize'])
 
 
 p2p = pix2Pix(conf) 
